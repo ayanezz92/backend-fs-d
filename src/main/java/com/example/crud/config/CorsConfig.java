042c375d27622ab05a886ef17ea8ece7f0ab90cd
Agustin_FS608 <agus.yanez@duocuc.cl>
2025-11-13 14:10:34 -0300

    Subiendo el enlace del backend a render

--- src/main/java/com/example/crud/config/CorsConfig.java
+++ src/main/java/com/example/crud/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("url-vercel")
+                        .allowedOrigins("https://frontend-fs-d-a4wc.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
